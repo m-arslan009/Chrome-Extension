3a603eada837c06813591c8aa484ef815f12c98b
Muhammad Arslan <iqbalarslan009@gmail.com>
2024-01-09 21:58:56 +0500

    write snipit to make text a link through js

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const ulEl = document.getElementById("ul-el");
 
 intputBtn.addEventListener("click", function() {
     myLead.push(inputEl.value);
+    renderLead();
+});
+
+function renderLead() {
+    let listItems = "";
     for(let i = 0; i < myLead.length; i++) {
-        // ulEl.innerHTML += "<li>" + myLead[i] + "</li>";
-        const li = document.createElement("li");
-        li.textContent = myLead[i];
-        ulEl.append(li);
+        listItems += "<li><a href = " + myLead[i] + " target = '_blank'>" + myLead[i] + "</a> </li>";
     }
 
-});
+    ulEl.innerHTML = listItems;
+    inputEl.value = '';
+}
 
