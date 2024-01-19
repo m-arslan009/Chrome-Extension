cee6857d7547cfcc8e087e5bc02d0b85a5604a97
Muhammad Arslan <iqbalarslan009@gmail.com>
2024-01-19 15:38:59 +0500

    add btn and set function to clear local storage as well as DOM

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+let myLead = [];
 const intputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
-let myLead = [];
 const ulEl = document.getElementById("ul-el");
+const items = JSON.parse(localStorage.getItem("myLeads"));
+const deleteBtn = document.getElementById("delete-btn");
+
 
 // console.log(typeof myLead)
 // convert js object to string that is necessery to store data in local storage
@@ -14,13 +17,18 @@ const ulEl = document.getElementById("ul-el");
 // console.log(typeof myLead)
 
 if(localStorage.getItem("myLeads")) {
-    let items = JSON.parse(localStorage.getItem("myLeads"));
     myLead = items;
     renderLead();
 }
 
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLead = [];
+    renderLead();
+})
+
 intputBtn.addEventListener("click", function() {
-    myLead.push(inputEl.value); // Now `push()` should work as expected
+    myLead.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads", JSON.stringify(myLead));
     renderLead();
